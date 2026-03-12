8c33d38023edde926c63784a4bc179096ac09f0f
gatogato999 <ggaattoo38@gmail.com>
2026-03-12 07:16:25 +0200

    can handle many tags

--- blogrenderer.go
+++ blogrenderer.go
@@ -11,8 +11,26 @@ type Post struct {
 }
 
 func Render(buf io.Writer, post Post) error {
-	_, err := fmt.Fprintf(buf, `<h1>%s</h1>
-<p>%s</p>
-Tags: <ul><li>%s</li><li>%s</li></ul>`, post.Title, post.Description, post.Tags[0], post.Tags[1])
-	return err
+	_, err := fmt.Fprintf(buf, `<h1>%s</h1> <p>%s</p>`, post.Title, post.Description)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprint(buf, `Tags: <ul>`)
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range post.Tags {
+		_, err := fmt.Fprintf(buf, `<li>%s</li>`, tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprint(buf, `</ul>`)
+	if err != nil {
+		return err
+	}
+	return nil
 }
