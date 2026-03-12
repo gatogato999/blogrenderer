d83f1023f9a07a59c101ad1f0ed56484f03d5db0
gatogato999 <ggaattoo38@gmail.com>
2026-03-12 07:01:27 +0200

    added: description and tag hardcoded

--- blogrenderer.go
+++ blogrenderer.go
@@ -11,6 +11,8 @@ type Post struct {
 }
 
 func Render(buf io.Writer, post Post) error {
-	_, err := fmt.Fprintf(buf, `<h1>%s</h1>`, post.Title)
+	_, err := fmt.Fprintf(buf, `<h1>%s</h1>
+<p>%s</p>
+Tags: <ul><li>%s</li><li>%s</li></ul>`, post.Title, post.Description, post.Tags[0], post.Tags[1])
 	return err
 }
